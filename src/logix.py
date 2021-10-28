87b74bb27532ad127ef93d75074ab75b87c36028
Carson Powers <crpowers0@gmail.com>
2021-10-27 22:32:45 -0500

    made draw_gate more readable, added draw_node

--- src/logix.py
+++ src/logix.py
@@ -52,8 +52,10 @@ class Editor:
 
         # Create Sidebar and Diagram widgets (Frame just holds the Canvas inside it)
         self.sidebar = tk.LabelFrame(self.window, text="Objects")
-        self.gatebar = tk.LabelFrame(self.sidebar, text = "Gates")
-        self.inputbar = tk.LabelFrame(self.sidebar, text = "Inputs")
+        self.gate_frame = tk.LabelFrame(self.sidebar, text = "Gates")
+        self.gate_buttons = []
+        self.input_frame = tk.LabelFrame(self.sidebar, text = "Inputs")
+        self.input_buttons = []
 
         self.frame = tk.LabelFrame(self.window, text="Diagram")
         self.diagram = tk.Canvas(self.frame, bg = "#808080")
@@ -64,8 +66,8 @@ class Editor:
         # Create table to hold each tk button, create table to hold objects
         # Create dictionary loaded_assets to hold loaded image for each type of object
         # *Avoids garbage collection and helps to reference tag names*
-        self.buttons = []
         self.objects = []
+        self.nodes = []
         self.edges = []
         self.loaded_assets = {} # key = title, value = asset
 
@@ -82,9 +84,9 @@ class Editor:
             asset = ImageTk.PhotoImage(Image.open(filename).resize(gate_dimensions))
             self.loaded_assets[title] = asset
 
-            button = tk.Button(self.gatebar, text = title, height = 1, width = 10)
-            button.bind("<ButtonPress-1>", self.gate_handler)
-            self.buttons.append(button)
+            button = tk.Button(self.gate_frame, text = title, height = 1, width = 10)
+            button.bind("<ButtonPress-1>", self.draw_gate)
+            self.gate_buttons.append(button)
         
         # Bind diagram to zoom/pan functions
         self.diagram.bind("<MouseWheel>", self.do_zoom)
@@ -104,52 +106,67 @@ class Editor:
         self.frame.columnconfigure(0, weight=1)
 
         # Add object buttons to the sidebar
-        for i in range(len(self.buttons)):
-            self.buttons[i].grid(row = i, column = 0, sticky = "EW")
+        for i in range(len(self.gate_buttons)):
+            self.gate_buttons[i].grid(row = i, column = 0, sticky = "EW")
         # Add all other widgets to Editor grid
         self.diagram.grid(row = 0, column = 0, sticky = "NSEW")
         self.sidebar.grid(row = 0, column = 0, sticky = "NS")
-        self.gatebar.grid(row = 0, column = 0, sticky = "NSEW")
-        self.inputbar.grid(row = 1, column = 0, sticky = "NSEW")
+        self.gate_frame.grid(row = 0, column = 0, sticky = "NSEW")
+        self.input_frame.grid(row = 1, column = 0, sticky = "NSEW")
         self.frame.grid(row = 0, column = 1, sticky = "NSEW")
 
 
-    def gate_handler(self, event):
-        """Handle gate button click and create respective gate and nodes on the diagram."""
+    def draw_node(self, coords, color, type, gate_id):
+        """
+        Create a node on the diagram and appropriately tag it.
+        
+        coords : table
+                 x0, y0, x1, y1 position (pixels)
+        color : string
+                node fill color (hexadecimal)
+        type : string
+               type of node ("input" or "output")
+        gate_id: tagOrId
+                 integer id of gate node is being attached to
+        """
+
+        # Create node at coords with color
+        x0, y0, x1, y1 = coords
+        node = self.diagram.create_oval(x0, y0, x1, y1, fill = color)
+
+        # Add node tags
+        self.diagram.addtag_withtag(type, node)
+        self.diagram.addtag_withtag("gate" + str(gate_id), node)
+        self.diagram.tag_raise(node)
+        self.nodes.append(node)
+
+
+    def draw_gate(self, event):
+        """
+        Handle gate button click and create respective gate and nodes on the diagram.
+        Appropriately tag gate.
+        """
 
         title = event.widget['text']
         num_inputs = self.object_data["gates"]["gate_types"][title]
-        node_fill = self.object_data["gates"]["node_fill"]
+        node_fill_color = self.object_data["gates"]["node_fill"]
 
         gate = self.diagram.create_image(0, 0, image = self.loaded_assets[title])
         self.diagram.tag_raise(gate)
         self.objects.append(gate)
 
         # Create input nodes
-        # TODO: For the love of god make this cleaner
         if num_inputs == 1:
-            x0, y0, x1, y1 = self.object_data["gates"]["input_node_position"]
-            node = self.diagram.create_oval(x0, y0, x1, y1, fill = node_fill)
-            self.diagram.addtag_withtag("input", node)
-            self.diagram.addtag_withtag("gate" + str(gate), node)
-            self.diagram.tag_raise(node)
-            self.objects.append(node)
+            coords = self.object_data["gates"]["input_node_position"]
+            self.draw_node(coords, node_fill_color, "input", gate)
         else:
-            for i in range(len(self.object_data["gates"]["two_input_node_positions"])):
-                x0, y0, x1, y1 = self.object_data["gates"]["two_input_node_positions"][i]
-                node = self.diagram.create_oval(x0, y0, x1, y1, fill = node_fill)
-                self.diagram.addtag_withtag("input", node)
-                self.diagram.addtag_withtag("gate" + str(gate), node)
-                self.diagram.tag_raise(node)
-                self.objects.append(node)
+            for i in range(num_inputs):
+                coords = self.object_data["gates"]["two_input_node_positions"][i]
+                self.draw_node(coords, node_fill_color, "input", gate)
         
         # Create output node
-        x0, y0, x1, y1 = self.object_data["gates"]["output_position"]
-        output = self.diagram.create_oval(x0, y0, x1, y1, fill = node_fill)
-        self.diagram.addtag_withtag("output", output)
-        self.diagram.addtag_withtag("gate" + str(gate), output)
-        self.diagram.tag_raise(output)
-        self.objects.append(output)
+        coords = self.object_data["gates"]["output_position"]
+        self.draw_node(coords, node_fill_color, "output", gate)
 
     
     def contains_xy(self, coords, x, y):
@@ -181,6 +198,7 @@ class Editor:
         return(False)
 
 
+    # TODO: Refine what qualifies as node state
     def check_grab_state(self, x, y):
         """
         Determine if (x,y) is within coordinates bounding an object, a node or just the diagram.
@@ -197,21 +215,21 @@ class Editor:
         -------
         state : If (x,y) was within coordinates bounding an object, node, or diagram
         """
-        # Loop through each objects, newest to oldest (Top to bottom layer)
+        # Loop through nodes, newest to oldest
+        for node in reversed(self.nodes):
+            # Get node coordinates
+            coords = self.diagram.bbox(node)
+            if self.contains_xy(coords, x, y):
+                self.grabbed_object = node
+                return("node")
+        # Loop through each objects, newest to oldest
         for object in reversed(self.objects):
             # Get object coordinates
             coords = self.diagram.bbox(object)
-
             if self.contains_xy(coords, x, y):
                 self.grabbed_object = object
-                tags = self.diagram.gettags(object)
-                for tag in tags:
-                    #Only allow "tag" state from outputs!
-                    if tag == "output":
-                        return("node")
-                    elif tag == "input":
-                        return("canvas")
                 return("object")
+        # If not grabbing a node or object, return state canvas
         return("canvas")
 
 
@@ -239,7 +257,7 @@ class Editor:
             c_x, c_y = self.find_center_coords(self.diagram.coords(self.grabbed_object))
             self.temp_edge = self.diagram.create_line(c_x, c_y, c_x, c_y, width = 3)
 
-
+    # TODO: Refine what qualifies as a valid end node
     def up_handler(self, event):
         """
         Handle the <ButtonRelease-1> event and consequent object actions.
@@ -251,41 +269,37 @@ class Editor:
         y = int(self.diagram.canvasy(event.y))
 
         if self.state == "node":
-            # Complete edge if it ends on another node
             edge = self.temp_edge
             valid_edge = False
             # Find gate of starting node
             start_gate = self.diagram.gettags(self.grabbed_object)[1][4]
             # Check if mouse is over a valid final node
-            for object in reversed(self.objects):
-                # Filter out non-nodes
-                if self.is_node(object):
-                    # Filter out objects mouse isnt touching
-                    if self.contains_xy(self.diagram.bbox(object), x, y):
-                        #Find gate final node is attached to
-                        end_gate = self.diagram.gettags(object)[1][4]
-                        #Filter out final nodes on the same gate
-                        if not(start_gate == end_gate):
-                            #Final node found! Create final edge
-                            valid_edge = True
-                            self.edges.append(edge)
+            for node in reversed(self.nodes):
+                # Filter out objects mouse isnt touching
+                if self.contains_xy(self.diagram.bbox(node), x, y):
+                    #Find gate final node is attached to
+                    end_gate = self.diagram.gettags(node)[1][4]
+                    #Filter out final nodes on the same gate
+                    if not(start_gate == end_gate):
+                        #Final node found! Create final edge
+                        valid_edge = True
+                        self.edges.append(edge)
 
-                            #Adjust edge coords to final position
-                            x0, y0 = self.find_center_coords(self.diagram.coords(self.grabbed_object))
-                            x1, y1 = self.find_center_coords(self.diagram.coords(object))
-                            self.diagram.coords(edge, x0, y0, x1, y1)
+                        #Adjust edge coords to final position
+                        x0, y0 = self.find_center_coords(self.diagram.coords(self.grabbed_object))
+                        x1, y1 = self.find_center_coords(self.diagram.coords(node))
+                        self.diagram.coords(edge, x0, y0, x1, y1)
 
-                            #Create tags that describe the two nodes the edge conects
-                            start_tag = "start" + str(self.grabbed_object)
-                            end_tag = "end" + str(object)
-                            self.diagram.addtag_withtag(start_tag, edge)
-                            self.diagram.addtag_withtag(end_tag, edge)
-                            print(self.diagram.gettags(edge))
+                        #Create tags that describe the two nodes the edge conects
+                        start_tag = "start" + str(self.grabbed_object)
+                        end_tag = "end" + str(node)
+                        self.diagram.addtag_withtag(start_tag, edge)
+                        self.diagram.addtag_withtag(end_tag, edge)
             
             if valid_edge == False:
                 self.diagram.delete(self.temp_edge)
                             
-        #Reset grab variables
+        # Reset grab variables
         self.object_grabbed = False
         self.temp_edge = False
         self.state = False
@@ -317,13 +331,11 @@ class Editor:
                 # Move nodes edges
                 # Find edges that start at node
                 for edge in self.diagram.find_withtag("start" + str(node)):
-                    print("found edge"+str(edge))
-                    #Find current coords and update
+                    # Find current coords and update
                     x0, y0, x1, y1 = self.diagram.coords(edge)
                     self.diagram.coords(edge, x0 + diff_x, y0 + diff_y, x1, y1)
                 # Find edges that end at node
                 for edge in self.diagram.find_withtag("end" + str(node)):
-                    print("found edge"+str(edge))
                     # Find current coords and update
                     x0, y0, x1, y1 = self.diagram.coords(edge)
                     self.diagram.coords(edge, x0, y0, x1 + diff_x, y1 + diff_y)
